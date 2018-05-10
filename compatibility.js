d9a7f811b4d9b155ed5687ea5b9a6659dd33acf2
finale11 <ttxx.bnb1@gmail.com>
2018-05-10 16:21:40 +0800

    add function

--- compatibility.js
+++ compatibility.js
@@ -1,15 +1,71 @@
+//获取非行内样式（兼容问题）
+function getStyle(obj, attr) {             //获取非行间样式，obj是对象，attr是值
+    if (obj.currentStyle) {                //针对ie获取非行间样式
+        return obj.currentStyle[attr];
+    } else {
+        return getComputedStyle(obj, false)[attr];   //针对非ie
+    }
+}
+// getStyle()
+
 // event事件对象获取
 // IE中:        window.event
 // 正常浏览器中: 对象on事件 = function(event){}
 // 兼容方式:
-function fn(eve) {
-    var e = eve || window.event;
-}
-
-// 阻止事件冒泡
-e.stopPropagation();
-e.cancelBubble = true;        //兼容IE
+// function fn(eve) {
+// var e = eve || window.event;
+// }
 
 // 得知键盘按下哪个键: keyCode
-var e = eve || event
-var keyC = e.keyCode || e.which
+// var e = eve || event
+// var keyC = e.keyCode || e.which
+
+// 阻止事件冒泡
+// e.stopPropagation();
+// e.cancelBubble = true;        //兼容IE
+function stopBubble(a) {
+    if (a.stopPropagation) {
+        a.stopPropagation()
+    } else {
+        a.cancelBubble = true;
+    }
+}
+
+//阻止默认事件
+function stopDefault(b) {
+    if (b.preventDefault) {
+        b.preventDefault()
+    } else {
+        b.returnValue = false;
+    }
+}
+// 调用
+// document.oncontextmenu = function (eve) {
+//     var e = eve || window.event
+//     stopDefault(e)
+// }
+
+// 事件监听操作
+var myEvent = {
+    add: function (ele, myeve, fn) {
+        if (ele.addEventListener) {
+            ele.addEventListener(myeve, fn, false);
+        } else if (ele.attachEvent) {
+            ele.attachEvent('on' + myeve, fn)
+        } else {
+            ele['on' + myeve] = fn;
+        }
+    },
+    remove: function (ele, myeve, fn) {
+        if (ele.removeEventListener) {
+            ele.removeEventListener(myeve, fn)
+        } else if (ele.detachEvent) {
+            ele.detachEvent('on' + myeve, fn)
+        } else {
+            ele['on' + myeve] = null;
+        }
+    }
+}
+// 调用
+// myEvent.add(obox, 'click', function () { })
+// myEvent.remove(obox, 'click', fn)
--- model.js
+++ model.js
@@ -1,9 +1,30 @@
-//获取非行内样式（兼容问题）
-function getStyle(obj, attr) {             //获取非行间样式，obj是对象，attr是值
-    if (obj.currentStyle) {                //针对ie获取非行间样式
-        return obj.currentStyle[attr];
+// 补零
+function zero(n) {
+    if (n < 10 || n.length < 2) {
+        return '0' + n;
     } else {
-        return getComputedStyle(obj, false)[attr];   //针对非ie
+        return n;
     }
 }
-getStyle()
+
+// 格式化日期
+function myDate() {
+    var d = new Date();
+    var y = d.getFullYear();
+    var m1 = d.getMonth();
+    var mydate = d.getDate();
+    var day = d.getDay()
+    var h = d.getHours()
+    var m2 = d.getMinutes()
+    var s = d.getSeconds()
+    switch (day) {
+        case 0: day = '星期日'; break;
+        case 1: day = '星期一'; break;
+        case 2: day = '星期二'; break;
+        case 3: day = '星期三'; break;
+        case 4: day = '星期四'; break;
+        case 5: day = '星期五'; break;
+        case 6: day = '星期六'; break;
+    }
+    return y + '年' + zero(m1 + 1) + '月' + zero(mydate) + '日 ' + day + ' ' + zero(h) + ':' + zero(m2) + ':' + zero(s);
+}
