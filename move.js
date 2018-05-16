8c75872458163d204655f1a2b9a02739f96b32e3
lic2112 <ttxx.bnb1@gmail.com>
2018-05-16 11:23:49 +0800

    update move.js

--- move.js
+++ move.js
@@ -1,31 +1,33 @@
-// 不同元素多属性运动
-function move(ele, attr, target) {
+// 链式运动
+function move(ele, obj, fn) {
     clearInterval(ele.timer)
     ele.timer = setInterval(() => {
-        if (attr == 'opacity') {
-            var iNow = getStyle(ele, attr) * 100
-        } else {
-            var iNow = parseInt(getStyle(ele, attr))
-        }
-        var speed = (target - iNow) / 7
-        speed = speed < 0 ? Math.floor(speed) : Math.ceil(speed)
-        if (Math.abs(target - iNow) < 1) {
-            clearInterval(ele.timer)
-        } else {
+        var onOff = true;
+        for (var attr in obj) {
+            var iNow = attr == 'opacity' ? getStyle(ele, attr) * 100 : parseInt(getStyle(ele, attr))
+            var speed = (obj[attr] - iNow) / 7
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed)
+            if (!Math.abs(obj[attr] - iNow) < 1) {
+                onOff = false
+            }
             iNow += speed
             if (attr == 'opacity') {
-                ele.style.opacity = iNow / 100
+                ele.style.opacity = iNow / 100;
                 ele.style.filter = 'alpha(opacity=' + iNow + ')'
             } else {
                 ele.style[attr] = iNow + 'px'
+            }
+            if (onOff) {
+                clearInterval(ele.timer)
+                if (fn) fn()
             }
         }
     }, 30);
 }
 function getStyle(obj, attr) {
-    if (obj.currentStyle) {
-        return obj.currentStyle[attr]
-    } else {
+    if (getComputedStyle) {
         return getComputedStyle(obj, false)[attr]
+    } else {
+        return obj.currentStyle[attr]
     }
 }
